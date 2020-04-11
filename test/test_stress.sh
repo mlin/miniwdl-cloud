be7c079b21f1925405b78649067b1b41fe9e1437
Mike Lin <dna@mlin.net>
2020-04-11 12:30:43 -1000

    add scheduler stress test

---
+++ test/test_stress.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# Run a lot of small jobs
+set -euxo pipefail
+
+cat << "EOF" > ~/test_stress.wdl
+version 1.0
+
+workflow stress_test {
+  input {
+    Int num_calls
+  }
+  scatter (i in range(num_calls)) {
+    call stressor
+  }
+
+  call postprocessor {
+    input:
+    numerator = stressor.numerator,
+    denominator = stressor.denominator
+  }
+  output {
+    Float estimate = postprocessor.estimate
+  }
+}
+
+task stressor {
+  input {
+    Int samples = 10000000
+  }
+  command <<<
+    set -euxo pipefail
+    python3 -c "
+    import random
+    random.seed()
+    inside = 0
+    for _ in range(~{samples}):
+      x = random.random()
+      y = random.random()
+      if x*x + y*y <= 1.0:
+        inside += 1
+    print(inside)" > numerator.txt
+  >>>
+  output {
+    File numerator = "numerator.txt"
+    File denominator = write_lines([samples])
+  }
+  runtime {
+    cpu: 1
+    docker: "continuumio/miniconda3"
+  }
+}
+
+task postprocessor {
+  input {
+    Array[File] numerator
+    Array[File] denominator
+  }
+  command <<<
+    set -euxo pipefail
+    ADDER="import sys; print(sum(int(line.strip()) for line in sys.stdin if line.strip()))"
+    inside=$(python3 -c "$ADDER" < <(cat "~{write_lines(numerator)}" | xargs cat))
+    N=$(python3 -c "$ADDER" < <(cat "~{write_lines(denominator)}" | xargs cat))
+    python3 -c "print(4.0*float(${inside})/float(${N}))"
+  >>>
+  output {
+    Float estimate = read_float(stdout())
+  }
+  runtime {
+    cpu: 1
+    docker: "continuumio/miniconda3"
+  }
+}
+EOF
+miniwdl check ~/test_stress.wdl
+time miniwdl run ~/test_stress.wdl \
+  num_calls=1000 stressor.samples=100000000 \
+  --dir /mnt/shared/runs
