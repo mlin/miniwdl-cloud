6990bc9531afce1b1e7dd45d6a727315664aa753
Mike Lin <dna@mlin.net>
2020-08-02 14:13:27 -1000

    upgrade base AMI to Ubuntu 20.04 (#4)

--- terraform/aws/monolith/main.tf
+++ terraform/aws/monolith/main.tf
@@ -62,7 +62,7 @@ resource "aws_spot_instance_request" "monolith" {
     # run ansible playbook
     inline = [
       "sudo add-apt-repository --yes universe",
-      "sudo apt-add-repository --yes --update ppa:ansible/ansible",
+      "sudo apt-get -qq update",
       "sudo apt-get -qq install -y python3-pip ansible",
       "ansible-playbook --connection=local -i 'localhost,'  --extra-vars 'ansible_python_interpreter=auto public_key_path=~/${basename(var.public_key_path)} lustre_dns_name=${module.common.lustre_dns_name} s3_export_path=s3://${var.s3bucket}/${var.outputs_prefix} miniwdl_branch=${var.miniwdl_branch}' ~/ansible/aws_monolith.yml"
     ]
